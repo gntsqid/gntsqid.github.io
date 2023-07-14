3198c225087cf76d7afb1bcf35efb2dafac0df8d
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-07-14 10:50:40 -0700

    Update tool-code.js

--- scripts/tool-code.js
+++ scripts/tool-code.js
@@ -1,5 +1,8 @@
 function copyCode() {
-  const code = document.getElementById('code').innerText;
+  let code = document.getElementById('code').innerText;
+  code = code.split('\n').map(line => line.trim()).join('\n');
   navigator.clipboard.writeText(code);
 }
+
+
 
