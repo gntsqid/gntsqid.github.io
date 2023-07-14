235eac5d579a1f9076360e94f26489a2c5f1ea57
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-07-14 14:08:26 -0700

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -5,3 +5,26 @@ function highlightCode(codeBlock, language) {
 
   code.classList.add(languageCode);
 }
+
+function copyCodeToClipboard() {
+  var codeBlock = document.querySelector(".code-container");
+  var code = codeBlock.querySelector(".code");
+
+  var copyText = code.textContent;
+  var copyTextElement = document.createElement("input");
+  copyTextElement.value = copyText;
+  copyTextElement.type = "text";
+  copyTextElement.style.display = "none";
+
+  document.body.appendChild(copyTextElement);
+  copyTextElement.select();
+  document.execCommand("copy");
+  copyTextElement.remove();
+}
+
+var copyButton = document.createElement("button");
+copyButton.textContent = "Copy";
+copyButton.onclick = copyCodeToClipboard;
+
+var codeContainer = document.querySelector(".code-container");
+codeContainer.appendChild(copyButton);
