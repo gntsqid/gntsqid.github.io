fdf671008cde8749d405ccdc4c8038459dca4dd3
gntsqid <sjlang.805@gmail.com>
2023-08-24 07:28:20 -0700

    Reconfigured the directory to specifical target the NETSEC club as a demo. NOTE: something with the rss feed is broken

--- scripts/circle-menu.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu-button').addEventListener('click', function() {
-    var menuItems = document.getElementById('menu-items');
-    if (menuItems.style.display === "none") {
-        menuItems.style.display = "block";
-    } else {
-        menuItems.style.display = "none";
-    }
-});
-
