56f182f8da9feb3bf76bc53a2f3c47a15a8d8416
gntsqid <sjlang.805@gmail.com>
2023-08-28 20:09:14 -0700

    Possible fix for json button.

--- scripts/fetchCTF.js
+++ scripts/fetchCTF.js
@@ -1,7 +1,7 @@
 var input = document.getElementByID("myTextBox")
 var save = document.getElementByID("save")
 
-jsonBtn.addEventListener("click", function()
+save.addEventListener("click", function()
   {
   var data = 
     }
