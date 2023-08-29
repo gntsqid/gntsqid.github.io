672849bb882c6ad45cf518299b8c26c61fa68785
gntsqid <sjlang.805@gmail.com>
2023-08-28 20:04:46 -0700

    Started doing some ctf database stuff

---
+++ scripts/fetchCTF.js
@@ -0,0 +1,11 @@
+var input = document.getElementByID("myTextBox")
+var save = document.getElementByID("save")
+
+jsonBtn.addEventListener("click", function()
+  {
+  var data = 
+    }
+    "input":input.value
+    }
+  jsonText.innerHTML = JSON.stringify(data)
+  })
