b8d125127bd93e77f73147a801f8baf27687ee97
gntsqid <sjlang.805@gmail.com>
2025-05-11 17:05:17 -0700

    Starting Fresh - Odin Project

--- scripts/tool-code.js
+++
@@ -1,8 +0,0 @@
-function copyCode() {
-  let code = document.getElementById('code').innerText;
-  code = code.split('\n').map(line => line.trim()).join('\n');
-  navigator.clipboard.writeText(code);
-}
-
-
-
